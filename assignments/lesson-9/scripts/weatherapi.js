5608fe4759956ce508c3dc8f0411b05c4d8931f3
Alexander <bylayer11@gmail.com>
2018-06-23 08:43:07 -0500

    api test
    upload

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,14 +1,14 @@
 window.onload = function program() {
        var weatherRequest = new XMLHttpRequest();
-       var requrl = "https://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=7d55d341a1dc275dc55a79e893b5e150";
-       weatherRequest.open('GET', requrl, true);
+       var req = "https://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=7d55d341a1dc275dc55a79e893b5e150";
+       weatherRequest.open('GET', req, true);
        weatherRequest.send();
        weatherRequest.onload = function () {
            var weatherData = JSON.parse(weatherRequest.responseText);
            console.log(weatherData);
-           var currentw = weatherData.list[0].main.temp;
-           var converty = ((currentw - 273.15) * 1.8) + 32;
-           document.getElementById("current-temp").innerHTML = converty.toFixed(2);
+           var currentweather = weatherData.list[0].main.temp;
+           var convert = ((currentweather - 273.15) * 1.8) + 32;
+           document.getElementById("current-temp").innerHTML = convert.toFixed(2);
 
        }
    }
