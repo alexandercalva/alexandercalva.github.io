f25c654d1861c2356d2f4db67c2b2e5c3b677fc6
Alexander <bylayer11@gmail.com>
2018-06-23 08:25:16 -0500

    api
    json

--- assignments/lesson-9/hero.js
+++ assignments/lesson-9/hero.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 {
   "squadName" : "Super hero squad",
   "homeTown" : "Metro City",
@@ -38,4 +39,46 @@
       ]
     }
   ]
+=======
+{
+  "squadName" : "Super hero squad",
+  "homeTown" : "Metro City",
+  "formed" : 2016,
+  "secretBase" : "Super tower",
+  "active" : true,
+  "members" : [
+    {
+      "name" : "Molecule Man",
+      "age" : 29,
+      "secretIdentity" : "Dan Jukes",
+      "powers" : [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name" : "Madame Uppercut",
+      "age" : 39,
+      "secretIdentity" : "Jane Wilson",
+      "powers" : [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name" : "Eternal Flame",
+      "age" : 1000000,
+      "secretIdentity" : "Unknown",
+      "powers" : [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+>>>>>>> d9e083e298d92df18fe28ded4376bb7abf65d526
 }
