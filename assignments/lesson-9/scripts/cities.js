430450cf69f5b182567d960b5ca5c6b12832d519
alexandercalva <38995873+alexandercalva@users.noreply.github.com>
2018-06-23 23:36:43 -0500

    udapte cities
    js

--- assignments/lesson-9/scripts/cities.js
+++ assignments/lesson-9/scripts/cities.js
@@ -1,6 +1,30 @@
 window.onload = function program() {
-
-  var towndata = new XMLHttpRequest();
+  
+     var weatherRequest = new XMLHttpRequest();
+       var req = "https://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=7d55d341a1dc275dc55a79e893b5e150";
+       weatherRequest.open('GET', req, true);
+       weatherRequest.send();
+       weatherRequest.onload = function () {
+           var weatherData = JSON.parse(weatherRequest.responseText);
+           console.log(weatherData);
+           var currentweather = weatherData.list[0].main.temp;
+           var convert = ((currentweather - 273.15) * 1.8) + 32;
+           document.getElementById("current-temp").innerHTML = convert.toFixed(2);
+		   document.getElementById("description").innerHTML = weatherData.list[0].weather[0].description;
+		   document.getElementById("highT").innerHTML = weatherData.list[0].main.temp_max;
+		   document.getElementById("lowT").innerHTML = weatherData.list[0].main.temp_min;
+	       var s = weatherData.list[0].wind.speed;
+		   document.getElementById("speed").innerHTML = s;
+		   var icon = weatherData.list[0].weather[0].icon;
+           var icon_path = "https://openweathermap.org/img/w/" + icon + ".png";
+           document.getElementById("icon").src = icon_path;
+		    
+		   
+		   
+	   }  
+      
+  
+   var towndata = new XMLHttpRequest();
   var requrl = "https://byui-cit230.github.io/weather/data/towndata.json";
   towndata.open('GET', requrl, true);
   towndata.send();
@@ -15,7 +39,7 @@ window.onload = function program() {
     document.getElementById("f-event0").innerHTML = obtainedata.towns[0].events[0];
     document.getElementById("f-event1").innerHTML = obtainedata.towns[0].events[1];
     document.getElementById("f-event2").innerHTML = obtainedata.towns[0].events[2];
- 
+   
     document.getElementById("greenvilleName").innerHTML = obtainedata.towns[1].name;
     document.getElementById("greenvilleMotto").innerHTML = obtainedata.towns[1].motto;
     document.getElementById("greenvilleYear").innerHTML = obtainedata.towns[1].yearFounded;
@@ -25,7 +49,7 @@ window.onload = function program() {
     document.getElementById("g-event1").innerHTML = obtainedata.towns[1].events[1];
     document.getElementById("g-event2").innerHTML = obtainedata.towns[1].events[2];
     document.getElementById("g-event3").innerHTML = obtainedata.towns[1].events[3];
-
+  
     document.getElementById("springfieldName").innerHTML = obtainedata.towns[3].name;
     document.getElementById("springfieldMotto").innerHTML = obtainedata.towns[3].motto;
     document.getElementById("springfieldYear").innerHTML = obtainedata.towns[3].yearFounded;
@@ -36,4 +60,8 @@ window.onload = function program() {
     document.getElementById("springfieldEvent2").innerHTML = obtainedata.towns[3].events[2];
     
  }
-}
+    
+ 
+
+ }
+ 
