c65f4a8022b6ba93c39f2db9b57302f7fc2e264d
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2019-03-15 17:46:43 -0500

    js folder
    update

---
+++ class_javascript/js/classes.js
@@ -0,0 +1,24 @@
+        function changeBackground(color) {
+			var element = document.querySelector(".box");
+			element.style.background = color;
+			element.style.color = "white";
+        }   
+		function bg(){
+			var element = document.querySelector(".bg");
+			element.style.background = "#c45234";
+		}
+		function bor(){
+			var element = document.querySelector(".fade");
+			element.style.border = "1px solid blue";
+		}
+		function pad() {
+			var element = document.querySelector("#padi");
+			element.style.padding = "50px";
+		}
+		function siz(){
+			var element = document.querySelector(".dim");
+			element.style.width="5em";
+			element.style.height="5em";
+			element.style.background="powderblue";
+			element.style.color="red";
+		}
